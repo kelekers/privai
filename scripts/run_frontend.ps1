217b981657cded23be27681a49d9936ff4121160
Kelekers <abhineztik@gmail.com>
2026-05-14 12:01:43 +0700

    Timpa

--- scripts/run_frontend.ps1
+++
@@ -1,9 +0,0 @@
-﻿Set-Location "D:\Lomba\privai\frontend"
-
-if (-Not (Test-Path ".\node_modules")) {
-    Write-Host "node_modules not found. Installing frontend dependencies..." -ForegroundColor Yellow
-    npm install
-}
-
-Write-Host "Starting PrivAI frontend at http://localhost:5173" -ForegroundColor Green
-npm run dev
